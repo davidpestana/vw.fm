18158009e19b88e9d6931ee98bbafb153051e247
David <david.pestana.perdomo@gmail.com>
2016-04-04 18:10:53 +0200

    layouts integration on public bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+#added bundles
+            new Sonata\SeoBundle\SonataSeoBundle(),
+#src bundles
             new AppBundle\AppBundle(),
+            new PublicBundle\PublicBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
